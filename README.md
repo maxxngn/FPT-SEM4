86b044f34bafd229c96feefd8494f7dae98acc49
maxxngn <nguyen-05@mail.ru>
2024-09-30 16:54:36 +0700

    Origin code

--- README.md
+++
@@ -1,2 +0,0 @@
-# FPT-SEM4
-project
--- src/main/java/com/example/fashion2/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.fashion2.repository;
-
-import com.example.fashion2.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
